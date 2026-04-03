f434b6b9d72a33838e324ea15f4563fad11c87d6
lohitakshsinha <lohitakshsinha@gmail.com>
2026-04-03 18:09:22 +0530

    Added Merge Sort implementation in C++ (Issue #1)

---
+++ Basic DSA/pattern/pattern_1.cpp
@@ -0,0 +1,81 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+
+Print the pattern in the function given to you. 
+
+just a practice question for brushing up your skills and reasoning abilities. */
+
+
+#include<iostream>
+using namespace std;
+
+void pattern(int n){
+  
+  // main loop
+
+  for(int i =1; i<=n; i++){
+  
+    // first part
+
+    for(int j=1; j<=i; j++){
+      cout<<"*";
+    }
+
+    // second part
+
+    for(int k=2*(n-i); k>0; k--){
+      cout<<" ";
+    }
+
+    // third part
+
+    for(int l=1; l<=i; l++){
+      cout<<"*";
+
+    }
+
+    cout<<endl;
+    
+  }
+    
+  //  Decreasing part
+  
+  // main loop
+
+  for(int i=n-1; i>0; i--){
+
+    for(int j=i; j>0; j--){
+      cout<<"*";
+    }
+
+    for(int k=0; k<2*(n-i); k++){
+      cout<<" ";
+    }
+
+    for(int l=i; l>0; l--){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+  
+}
+
+int main(){
+  int size;
+  cout<<"Enter the size of pattern"<<endl;
+  cin>>size;
+  pattern(size);
+  return 0;
+}
