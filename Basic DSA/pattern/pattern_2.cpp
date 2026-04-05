e668334ee9523684b6a819ebb016901d354498e1
Parth Sahay <parthsahay24@gmail.com>
2026-04-05 23:56:32 +0530

    solution pattern 1

---
+++ Basic DSA/pattern/pattern_2.cpp
@@ -0,0 +1,32 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5
+
+
+Constraints:
+1 <= n <= 100
+*/
+
+#include<iostream>
+using namespace std;
+
+void pattern(){
+    //your code here
+
+}
+int main(){
+  int size;
+  cout<<"Enter the size of pattern"<<endl;
+  cin>>size;
+  pattern(size);
+  return 0;
+}
