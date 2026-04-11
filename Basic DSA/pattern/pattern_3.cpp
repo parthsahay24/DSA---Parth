d66feb088a12bb1e56ce846b3d3bf8b0853f302e
Parth Sahay <parthsahay24@gmail.com>
2026-04-12 00:00:49 +0530

    pattern_3_ question added

---
+++ Basic DSA/pattern/pattern_3.cpp
@@ -0,0 +1,26 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+
+*****
+*   *
+*   *
+*   *
+*****
+
+
+Print the pattern in the function given to you. */
+
+
+#include<iostream>
+using namespace std;
+
+void pattern(int n){
+    //your code here
+}
+
+int main(){
+  int size=5;
+  pattern(size);
+  return 0;
+}
