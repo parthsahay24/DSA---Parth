39ef891c14993b8d109aa601d1b8859bb3444c66
Parth Sahay <parthsahay24@gmail.com>
2026-04-13 22:01:03 +0530

    solution to pattern 3

--- Basic DSA/pattern/pattern_3.cpp
+++ Basic DSA/pattern/pattern_3.cpp
@@ -17,6 +17,22 @@ using namespace std;
 
 void pattern(int n){
     //your code here
+    for(int i =0;i<n;i++){
+      for(int j=0;j<n;j++){
+        if(i==0 || i==n-1){
+          cout<<"*";
+        }
+        else{
+          if(j==0 || j==n-1){
+            cout<<"*";
+          }
+          else{
+            cout<<" ";
+          }
+        }
+      }
+      cout<<endl<<endl;
+    }
 }
 
 int main(){
