9ce0996cd87e8b0fd870f8955440b0795cf0496b
Parth Sahay <parthsahay24@gmail.com>
2026-05-01 23:51:32 +0530

    Hashing Question

---
+++ Basic DSA/Hashing/q1.cpp
@@ -0,0 +1,29 @@
+/*Highest Occurring Element in an Array
+
+Given an array nums of n integers, find the most frequent element in it i.e., 
+the element that occurs the maximum number of times. If there are multiple elements 
+that appear a maximum number of times, find the smallest of them.
+
+Please note that this section might seem a bit difficult without prior knowledge on 
+what hashing is, we will soon try to add basics concepts for your ease! If you know 
+the concepts already please go ahead to give a shot to the problem. Cheers!
+
+Example 1
+
+Input: nums = [1, 2, 2, 3, 3, 3]
+
+Output: 3
+
+Explanation: The number 3 appears the most (3 times). It is the most frequent element. */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n=5;
+    int arr[n] = {1, 2, 2, 3, 3};
+    count(arr, n);
+    return 0;
+}
+void count(int arr[], int n){;
+
+}
